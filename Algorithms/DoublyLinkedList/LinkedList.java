66bb739d1068b37d26c0930a1cab531a5a83d11c
ikeda042 <ikea042main@gmail.com>
2024-03-25 16:43:41 +0900

    feat: doubly linked list

---
+++ Algorithms/DoublyLinkedList/LinkedList.java
@@ -0,0 +1,47 @@
+package Algorithms.DoublyLinkedList;
+
+public class LinkedList<T extends Comparable<T>> {
+
+    private Node<T> head;
+    private Node<T> tail;
+    boolean isReversed = false;
+
+    public LinkedList(Node<T> head) {
+
+        this.head = head;
+        this.tail = null;
+
+    }
+
+    public void add(T data) {
+
+        Node<T> curNode = head;
+        Node<T> newNode = new Node<T>(data);
+        while (curNode.getNext() != null) {
+            curNode = curNode.getNext();
+        }
+        curNode.setNext(newNode);
+        newNode.setPrev(curNode);
+        tail = newNode;
+    }
+
+    public String toString() {
+        String result = "";
+        if (!isReversed) {
+            Node<T> currNode = head;
+            while (currNode.getNext() != null) {
+                result += currNode.getData() + "->";
+                currNode = currNode.getNext();
+            }
+            result += currNode.getData();
+        } else {
+            Node<T> currNode = tail;
+            while (currNode.getPrev() != null) {
+                result += currNode.getData() + "->";
+                currNode = currNode.getPrev();
+            }
+            result += currNode.getData();
+        }
+        return result;
+    }
+}
---
+++ Algorithms/DoublyLinkedList/Node.java
@@ -0,0 +1,38 @@
+package Algorithms.DoublyLinkedList;
+
+public class Node<T extends Comparable<T>> {
+
+    private T data;
+    private Node<T> next;
+    private Node<T> prev;
+
+    public Node(T data) {
+        this.data = data;
+        this.next = null;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+
+    public void setPrev(Node<T> prev) {
+        this.prev = prev;
+    }
+
+    public Node<T> getPrev() {
+        return this.prev;
+    }
+
+}
