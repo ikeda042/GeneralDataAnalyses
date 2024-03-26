3528056ef7022a8ad9d2bb3991b7242d26f1f0f6
ikeda042 <ikeda042main@gmail.com>
2024-03-27 03:11:45 +0900

    feat: BST sort java

---
+++ Algorithms/BSTSort/BST.java
@@ -0,0 +1,72 @@
+public class BST <T extends Comparable<T>>{
+
+    private  T key;
+    private BST left = null;
+    private BST right = null;
+
+    public BST(){
+    }
+
+    public BST(T key){
+        this.key = key;
+    }
+
+    public void insert(T val){
+        if (this.key.compareTo(val) < 0){
+            if (this.right == null){
+                this.right = new BST<T>(val);
+            }else{
+                this.right.insert(val);
+            }
+        }else{
+            if (this.left == null){
+                this.left = new BST<T>(val);
+            }else{
+                this.left.insert(val);
+            }
+        }
+    }
+
+    
+    public String inorder(BST root,String array){
+                if (root == null){
+                    return array;
+                }else{
+                return this.inorder(root.left, array) + root.key +","+ this.inorder(root.right, array);
+                }
+        }
+
+    public void BSTout(){
+        if (this.left != null){
+            this.left.BSTout();
+        }
+        System.out.println(this.key);
+        if (this.right != null){
+            this.right.BSTout();
+        }
+    }
+
+    public String BSTsort(T[] array){
+        BST out = new BST(array[0]);
+        for (int i = 0; i<array.length;i++){
+            out.insert(array[i]);
+        }
+        return "[" + out.inorder(out, "").substring(0, array.length*2-1) + "]";
+    }
+
+
+
+    public static void main(String[] args) {
+        BST bst = new BST<>();
+        String[] array = new String[26];
+        for (int i = 0; i < 26; i++) {
+            array[25-i] = "" + (char) (97+i);
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+        System.out.println(bst.BSTsort(array));
+    }
+
+}
