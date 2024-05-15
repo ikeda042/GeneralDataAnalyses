f441c0622b651bc850f4e505997fa47e8dab4164
ikeda042 <ikeda042main@gmail.com>
2024-05-15 10:39:24 +0900

    main

--- DataAnalyses/CellAnalyses/main.py
+++ DataAnalyses/CellAnalyses/main.py
@@ -111,8 +111,8 @@ peak_path_analysis = False
 # CellAnalysesを実行
 if __name__ == "__main__":
     for d in [
-        "DataAnalyses/CellAnalyses/dataset-space/SK25_LB_1mL 1_1.db",
-        "DataAnalyses/CellAnalyses/dataset-space/SK25_LB_1mL 2_1.db",
-        "DataAnalyses/CellAnalyses/dataset-space/SK25_LB_1mL 3_1.db",
+        "DataAnalyses/CellAnalyses/dataset-space/SK25_LB_3mL 1_1.db",
+        "DataAnalyses/CellAnalyses/dataset-space/SK25_LB_3mL 2_1.db",
+        "DataAnalyses/CellAnalyses/dataset-space/SK25_LB_3mL 3_1.db",
     ]:
         analyze_cells(f"{d}", morphology_analysis, peak_path_analysis, only_ph)
